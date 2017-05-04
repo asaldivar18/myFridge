cb45d86cd8bf771d4c918c814354a42fa709c192
asaldivar18 <asaldivar18@users.noreply.github.com>
2017-05-04 05:53:01 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php include("include/header.php"); ?>
+<?php include("include/dashboard.php");?>
+<section class="container">
+<button disabled class="mdl-button mdl-js-button mdl-button--raised" id="quickstart-sign-in">Sign in with Google</button>
+
+<div class="quickstart-user-details-container">
+  Firebase sign-in status: <span id="quickstart-sign-in-status">Unknown</span>
+  <div>Firebase auth <code>currentUser</code> object value:</div>
+  <pre><code id="quickstart-account-details">null</code></pre>
+  <div>Google OAuth Access Token:</div>
+  <pre><code id="quickstart-oauthtoken">null</code></pre>
+</section>
+
+
+<?php
+include("firebaseconn.php");
+include("include/footer.php"); ?>
