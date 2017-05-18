2789234c21e7a896a4b8e1a79eb79762df857ecd
Daniel <daniel_katz@hotmail.ca>
2017-05-18 12:03:10 -0700

    rofl

--- js/profile.js
+++ js/profile.js
@@ -52,6 +52,22 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
   }
 });
 
+function readURL(input) {
+var url = input.value;
+var ext = url.substring(url.lastIndexOf('.') + 1).toLowerCase();
+if (input.files && input.files[0]&& (ext == "gif" || ext == "png" || ext == "jpeg" || ext == "jpg")) {
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+        $('#img').attr('src', e.target.result);
+    }
+    reader.readAsDataURL(input.files[0]);
+}
+else{
+     $('#img').attr('src', '/assets/no_preview.png');
+  }
+}
+
 function toggleSignIn() {
   if (!firebase.auth().currentUser) {
     var provider = new firebase.auth.GoogleAuthProvider();
