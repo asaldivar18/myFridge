c02d3163b164317e98111409096e33b5023ab39d
Adrian Saldivar <adriansaldivar18@gmail.com>
2017-05-23 11:28:26 -0700

    sprint 3

--- js/main.js
+++ js/main.js
@@ -2,22 +2,36 @@
  * myFridge JS
  *
  */
-var gButton = document.getElementById("myButton");
 
-
+var list = []
 /**
  * getItem
  * method when add item form button pressed.
  */
+
 function getItem() {
-  var name = document.getElementById("name").value;
+  var name = document.getElementById("name").value
   var quantity = document.getElementById("quantity").value;
   var dateTo = new Date();
+  var unit = document.getElementById("unit").value;
+
   dateTo = document.getElementById("dateto").value;
+  if(validateForm(name, quantity)== false) {
+    return 0
+  } else {
+    var newref = writeUserData(name, quantity, unit, dateTo.toString());
+    $("#itemform").hide(1000);
+    $("#myFridge").show(1000);
+    $("#showfridge").show(1000)
+  }
+}
 
-  var newref = writeUserData(name, quantity, dateTo.toString());
-readUserData();
-
+function validateForm(name, quantity){
+  if (name == "" || quantity == ""){
+    return false
+  } else {
+    return true
+  }
 }
 
 function deleteItem(num) {
@@ -48,11 +62,53 @@ function deleteItem(num) {
     })
     readUserData();
   })
+
+
   editpencil.addEventListener('click', function(event) {
     itemName = document.getElementById('itemName' + num).innerHTML;
-    console.log(itemName)
+    quantity = document.getElementById(num).innerHTML;
+    expiryDate = document.getElementById("itemDate" +num).children[0].innerHTML
+
+    var name_DOM = document.getElementById('itemName' + num)
+    var quantity_DOM = document.getElementById(num)
+    var expiry_DOM = document.getElementById('itemDate' + num)
+    var buttons_DOM = document.getElementById("buttons"+ num)
+    var row_DOM = document.getElementById('itemNo'+num)
+
+    removeChild(name_DOM)
+    removeChild(quantity_DOM)
+    removeChild(expiry_DOM)
+    removeChild(buttons_DOM)
+
+    name_DOM.innerHTML = "<input type=\"text\" name=\"itemName\"value=\""+itemName+"\"  placeholder=\""+itemName+"\" required id=\"newName\"></input>";
+    quantity_DOM.innerHTML ="<input type=\"number\" name=\"itemName\" value=\""+parseInt(quantity)+"\"placeholder=\""+quantity+"\" required id=\"newQuantity\"></input>";
+    expiry_DOM.innerHTML =  "<input placeholder=\""+expiryDate+"\" value=\""+expiryDate+"\" name=\"expiryDate\" type=\"text\" onfocus=\"(this.type='date\')\" onblur=\"(this.type='text\')\" id=\"newDateto\">"
+    buttons_DOM.innerHTML = "<input type=\"button\" onclick=\"updateItem()\" value=\"Confirm\"></input>"
+
+    updateItem(num)
+
   })
 }
+
+function updateItem(num){
+  var itemName = document.getElementById("newName").value
+  var quantity = document.getElementById("newQuantity").value;
+  var dateTo = new Date();
+  var user = firebase.auth().currentUser;
+  var ref = firebase.database().ref('Fridge/' + user.uid)
+
+  dateTo = document.getElementById("newDateto").value;
+  console.log(list[num])
+
+
+}
+
+function removeChild(dom_element){
+  while (dom_element.firstChild) {
+    dom_element.removeChild(dom_element.firstChild)
+  }
+}
+
 
 /**
  * Creates new dom element of all items of Fridge.
@@ -71,12 +127,15 @@ function readUserData() {
       var childData = childSnapshot.val();
       var tr = document.createElement('tr');
       tr.id = "itemNo" + itemNo;
-      var html = "<td id=\"itemName" + itemNo + "\">" + childData.name + "</td><td id=\"" + itemNo + "\">" + childData.quantity + "</td><div class=\"progress\"><div class=\"progress-bar progress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:100%\">" + childData.health + "</div>/div></td><div class=\"remove\"><td align=\"right\"><a><i class=\"fa fa-pencil fa-2x\" id=\"editpencil" + itemNo + "\" aria-hidden=\"true\"> |</i><i id=\"delete" + itemNo + "\"class=\"fa fa-times  fa-2x\" aria-hidden=\"true\"></i></a></td></div>"
+      var html = "<td id=\"itemName" + itemNo + "\">" + childData.name + "</td><td id=\"" + itemNo + "\">" + childData.quantity + " " + childData.type
+      html +=  "</td><td><div id=\"itemDate" + itemNo+ "\" class=\"progress\"><div class=\"progress-bar progress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:100%\">" + childData.health
+      html += "</div>/div></td><div class=\"remove\"><td id=\"buttons"+itemNo+"\"align=\"right\"><a><i class=\"fa fa-pencil fa-2x\" id=\"editpencil" + itemNo
+      html += "\" aria-hidden=\"true\"> |</i><i id=\"delete" + itemNo + "\"class=\"fa fa-times  fa-2x\" aria-hidden=\"true\"></i></a></td></div>"
       tr.innerHTML = html;
       document.getElementById("itembody").appendChild(tr);
       deleteItem(itemNo)
       itemNo++;
-
+      list.push(key)
     })
   })
 }
@@ -87,13 +146,14 @@ function readUserData() {
  * writeUserData
  * pushes JSON object into firebase database.
  */
-function writeUserData(name, quantity, health) {
+function writeUserData(name, quantity, unit, health) {
   var user = firebase.auth().currentUser;
   ref = firebase.database().ref('Fridge/' + user.uid)
   if (user) {
     var justPushed = ref.push({
       name: name,
       quantity: quantity,
+      type: unit,
       health: health,
     });
 
@@ -110,12 +170,12 @@ function writeUserData(name, quantity, health) {
         })
       }
     })
-    $("#additemForm").reset();
+    readUserData();
     return justPushed;
   }
 }
 
-/**
+/***************************
  * Sign In/Sign Out
  * Function called when clicking the Login With Google/logout button
  */
@@ -146,31 +206,6 @@ function fbConn() {
   firebase.initializeApp(config);
 }
 
-// Result from Redirect auth flow.
-function getToken() {
-  firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-      // google token @see Google API
-      window.location.href = "index.html";
-      var token = result.credential.accessToken;
-    } else {}
-    // The signed-in user info.
-    var user = result.user;
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    if (errorCode === 'auth/account-exists-with-different-credential') {
-      alert('You have already signed up with a different auth provider for that email.');
-      // If you are using multiple auth providers on your app you should handle linking
-      // the user's accounts here.
-    } else {
-      console.error(error);
-    }
-  });
-}
 
 // Listening for auth state changes.
 function getAuthState() {
@@ -184,11 +219,10 @@ function getAuthState() {
       var isAnonymous = user.isAnonymous;
       var uid = user.uid;
       var providerData = user.providerData;
-      console.log(email);
-
-      document.getElementById("myButton").innerText = "Continue";
-
-
+      $("#myFridge").show(1000);
+      $("#showfridge").prop('disabled',true);
+      document.getElementById("fridgeName").innerHTML = "Fridge Contents";
+      readUserData();
     } else {
       window.location.href = "login.html";
     }
@@ -201,9 +235,9 @@ function getAuthState() {
  */
 function initApp() {
   fbConn();
-  getToken();
   getAuthState();
 }
+
 
 window.onload = function() {
   initApp();
