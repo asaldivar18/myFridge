c07762ada8158c7289247b2f85c7e5a0bbc25fe8
seph1234 <jtang127@gmail.com>
2017-05-24 15:53:09 -0700

    validation

---
+++ js/login.js
@@ -0,0 +1,58 @@
+var config = {
+  apiKey: "AIzaSyAivtwKsYOVkBxknHjMhbTIkjMj0MhSPdM",
+  authDomain: "myfridge-ff976.firebaseapp.com",
+  databaseURL: "https://myfridge-ff976.firebaseio.com",
+  projectId: "myfridge-ff976",
+  storageBucket: "myfridge-ff976.appspot.com",
+  messagingSenderId: "1076830760594"
+};
+firebase.initializeApp(config);
+
+const submit = document.getElementById("s1");
+const txtemail = document.getElementById("e1");
+const txtpwd = document.getElementById("p1");
+const resetpwd = document.getElementById("p2");
+
+submit.addEventListener('click', e => {
+  const email2 = txtemail.value;
+  const pwd = txtpwd.value
+  const auth = firebase.auth();
+  const promise = auth.signInWithEmailAndPassword(email2, pwd).catch(function(error) {
+      alert("You have entered an invalid email/password!");
+})});
+
+resetpwd.addEventListener('click', e => {
+  const email2 = txtemail.value;
+  const pwd = txtpwd.value
+  const auth = firebase.auth();
+  const promise = auth.sendPasswordResetEmail(email2).then(function() {
+    console.log("Password Reset Sent!");
+  }, function(error) {
+    console.log("Error");
+  });
+
+});
+
+firebase.auth().onAuthStateChanged(firebaseUser =>{
+  if(firebaseUser){
+    console.log(firebaseUser);
+    window.location.href = "index.html";
+  }else{
+  console.log("not logged in");
+  }
+  });
+
+/**
+ * Sign In/Sign Out
+ * Function called when clicking the Login With Google/logout button
+ */
+function toggleSignIn() {
+  if (!firebase.auth().currentUser) {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithRedirect(provider);
+  } else {
+    firebase.auth().signOut();
+    window.location.href = "login.html";
+  }
+}
--- js/main.js
+++ js/main.js
@@ -2,50 +2,169 @@
  * myFridge JS
  *
  */
-var gButton = document.getElementById("myButton");
 
-
+var list = []
 /**
  * getItem
  * method when add item form button pressed.
  */
+
 function getItem() {
-  var name = document.getElementById("name").value;
+  var name = document.getElementById("name").value
   var quantity = document.getElementById("quantity").value;
   var dateTo = new Date();
+  var unit = document.getElementById("unit").value;
+
   dateTo = document.getElementById("dateto").value;
-  writeUserData(name, quantity, dateTo.toString());
-  var node = document.getElementById("itembody");
-  while (node.firstChild){
-    node.removeChild(node.firstChild)
+  if(validateForm(name, quantity)== false) {
+    return 0
+  } else {
+    var newref = writeUserData(name, quantity, unit, dateTo.toString());
+    $("#itemform").hide(1000);
+    $("#myFridge").show(1000);
+    $("#showfridge").show(1000)
   }
-  readUserData();
 }
 
-function edit(){
+function validateForm(name, quantity){
+  if (name == "" || quantity == ""){
+    return false
+  } else {
+    return true
+  }
+}
+
+function deleteItem(num) {
+  var deletebutton = document.getElementById('delete' + num);
+  var editpencil = document.getElementById('editpencil' + num);
+  var user = firebase.auth().currentUser;
+  var ref = firebase.database().ref('Fridge/' + user.uid)
+
+  deletebutton.addEventListener('click', function(event) {
+    var itemName = document.getElementById("itemName" + num)
+    var ref = firebase.database().ref('Fridge/'+user.uid+'/'+list[num-1])
+    var r = confirm("Are you sure you want to remove " + itemName + "?");
+    if (r == true){
+      ref.update({
+        removed:true
+      })
+    } else {
+      return 0
+    }
+
+    ref2 = firebase.database().ref('Fridge/score/' + user.uid)
+    ref2.once('value', function(childSnapshot) {
+      try {
+        var scoreuser = childSnapshot.val().score
+        ref2.update({
+          score: scoreuser + 1
+        })
+      } catch (abc) {
+        ref2.update({
+          score: 1
+        })
+      }
+    })
+    readUserData();
+  })
+
+
+  editpencil.addEventListener('click', function(event) {
+    itemName = document.getElementById('itemName' + num).innerHTML;
+    quantity = document.getElementById(num).innerHTML;
+    expiryDate = document.getElementById("itemDate" +num).children[0].innerHTML
+
+    var name_DOM = document.getElementById('itemName' + num)
+    var quantity_DOM = document.getElementById(num)
+    var expiry_DOM = document.getElementById('itemDate' + num)
+    var buttons_DOM = document.getElementById("buttons"+ num)
+    var row_DOM = document.getElementById('itemNo'+num)
+
+    removeChild(name_DOM)
+    removeChild(quantity_DOM)
+    removeChild(expiry_DOM)
+    removeChild(buttons_DOM)
+
+    name_DOM.innerHTML = "<input type=\"text\" name=\"itemName\"value=\""+itemName+"\"  placeholder=\""+itemName+"\" required id=\"newName\"></input>";
+    quantity_DOM.innerHTML ="<input type=\"number\" name=\"itemName\" value=\""+parseInt(quantity)+"\"placeholder=\""+quantity+"\" required id=\"newQuantity\"></input>";
+    expiry_DOM.innerHTML =  "<input placeholder=\""+expiryDate+"\" value=\""+expiryDate+"\" name=\"expiryDate\" type=\"text\" onfocus=\"(this.type='date\')\" onblur=\"(this.type='text\')\" id=\"newDateto\">"
+    buttons_DOM.innerHTML = "<input id=\"edit"+num+"\" type=\"button\"  value=\"Confirm\"></input>"
+
+    document.getElementById("edit"+num).addEventListener('click', function(event){
+      var user = firebase.auth().currentUser;
+      var ref = firebase.database().ref('Fridge/'+user.uid+'/'+list[num-1])
+      var itemName = name_DOM.children[0].value
+      var quantity = quantity_DOM.children[0].value;
+      var dateTo = new Date();
+      dateTo = expiry_DOM.children[0].value;
+      ref.update({
+        name: itemName,
+        quantity: quantity,
+        health: dateTo,
+      })
+      readUserData()
+    })
+
+  })
+}
+
+function updateItem(num){
+  var itemName = document.getElementById("newName").value
+  var quantity = document.getElementById("newQuantity").value;
+  var dateTo = new Date();
+  var user = firebase.auth().currentUser;
+  var ref = firebase.database().ref('Fridge/' + user.uid+"/"+list[num-1])
+  dateTo = document.getElementById("newDateto").value;
+  ref.update({
+    name: itemName,
+    quantity: quantity,
+    health: dateTo
+  })
+  readUserData()
 
 }
+
+function removeChild(dom_element){
+  while (dom_element.firstChild) {
+    dom_element.removeChild(dom_element.firstChild)
+  }
+}
+
 
 /**
  * Creates new dom element of all items of Fridge.
  */
 function readUserData() {
+  list.length = 0
+  var node = document.getElementById("itembody");
+  while (node.firstChild) {
+    node.removeChild(node.firstChild)
+  }
   var user = firebase.auth().currentUser;
-  var ref = firebase.database().ref('Fridge/'+user.uid).orderByKey();
-  ref.once("value")
-    .then(function(snapshot){
-      snapshot.forEach(function(childSnapshot){
-        var key = childSnapshot.key;
-        var childData = childSnapshot.val();
+  var itemNo = 1;
+  var cmp = "name" //document.getElementById("DANIELTODO").value
+  var ref = firebase.database().ref('Fridge/' + user.uid).orderByChild("health");
+  ref.on("value", function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var key = childSnapshot.key;
+      var childData = childSnapshot.val();
+      if(!childData.removed){
         var tr = document.createElement('tr');
-        var html = "<td>"+childData.name+"</td><td>"+childData.quantity+"</td><div class=\"progress\"><div class=\"progress-bar progress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:100%\">Fresh</div>/div></td><div class=\"remove\"><td align=\"right\"><a><i class=\"fa fa-pencil fa-2x\" id=\"editpencil\" aria-hidden=\"true\"> |</i><i class=\"fa fa-times  fa-2x\" aria-hidden=\"true\"></i></a></td></div>"
+        tr.id = "itemNo" + itemNo;
+        var html = "<td id=\"itemName" + itemNo + "\">" + childData.name + "</td><td id=\"" + itemNo + "\">" + childData.quantity + " " + childData.type
+        html +=  "</td><td><div id=\"itemDate" + itemNo+ "\" class=\"progress\"><div class=\"progress-bar progress-bar-striped active\" role=\"progressbar\" aria-valuenow=\"100\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width:100%\">" + childData.health
+        html += "</div>/div></td><div class=\"remove\"><td id=\"buttons"+itemNo+"\"align=\"right\"><a><i class=\"fa fa-pencil fa-2x\" id=\"editpencil" + itemNo
+        html += "\" aria-hidden=\"true\"> |</i><i id=\"delete" + itemNo + "\"class=\"fa fa-times  fa-2x\" aria-hidden=\"true\"></i></a></td></div>"
         tr.innerHTML = html;
         document.getElementById("itembody").appendChild(tr);
-      })
+        deleteItem(itemNo)
+        itemNo++;
+        list.push(key)
+      }
+
     })
+  })
 }
-
-
 
 
 
@@ -53,21 +172,37 @@ function readUserData() {
  * writeUserData
  * pushes JSON object into firebase database.
  */
-function writeUserData(name, quantity, health) {
-  firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
-      firebase.database().ref('Fridge/' + user.uid).push({
-        name: name,
-        quantity: quantity,
-        health: health,
-      });
-    } else {
-      toggleSignIn();
-    }
-  });
+function writeUserData(name, quantity, unit, health) {
+  var user = firebase.auth().currentUser;
+  ref = firebase.database().ref('Fridge/' + user.uid)
+  if (user) {
+    var justPushed = ref.push({
+      name: name,
+      quantity: quantity,
+      type: unit,
+      removed: false,
+      health: health,
+    });
+
+    ref2 = firebase.database().ref('Fridge/score/' + user.uid)
+    ref2.once('value', function(childSnapshot) {
+      try {
+        var scoreuser = childSnapshot.val().score
+        ref2.update({
+          score: scoreuser + 1
+        })
+      } catch (vafa) {
+        ref2.update({
+          score: 1
+        })
+      }
+    })
+    readUserData();
+    return justPushed;
+  }
 }
 
-/**
+/***************************
  * Sign In/Sign Out
  * Function called when clicking the Login With Google/logout button
  */
@@ -78,7 +213,7 @@ function toggleSignIn() {
     firebase.auth().signInWithRedirect(provider);
   } else {
     firebase.auth().signOut();
-    window.location.href = "index.html";
+    window.location.href = "login.html";
   }
 }
 
@@ -98,31 +233,6 @@ function fbConn() {
   firebase.initializeApp(config);
 }
 
-// Result from Redirect auth flow.
-function getToken() {
-  firebase.auth().getRedirectResult().then(function(result) {
-    if (result.credential) {
-      // google token @see Google API
-      window.location.href = "index.html";
-      var token = result.credential.accessToken;
-    } else {}
-    // The signed-in user info.
-    var user = result.user;
-  }).catch(function(error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    var email = error.email;
-    var credential = error.credential;
-    if (errorCode === 'auth/account-exists-with-different-credential') {
-      alert('You have already signed up with a different auth provider for that email.');
-      // If you are using multiple auth providers on your app you should handle linking
-      // the user's accounts here.
-    } else {
-      console.error(error);
-    }
-  });
-}
 
 // Listening for auth state changes.
 function getAuthState() {
@@ -136,12 +246,12 @@ function getAuthState() {
       var isAnonymous = user.isAnonymous;
       var uid = user.uid;
       var providerData = user.providerData;
-      console.log(email);
-
-      document.getElementById("myButton").innerText = "Continue";
-
+      $("#myFridge").show(1000);
+      $("#showfridge").prop('disabled',true);
+      document.getElementById("fridgeName").innerHTML = "Fridge Contents";
+      readUserData();
     } else {
-      // User is signed out.
+      window.location.href = "login.html";
     }
   });
 }
@@ -152,14 +262,10 @@ function getAuthState() {
  */
 function initApp() {
   fbConn();
-  getToken();
   getAuthState();
 }
+
 
 window.onload = function() {
   initApp();
 };
-
-                        
-                                       
-                                       
